7a65d0adb09631abd00c4240bf85365288040452
Crassus666 <74691874+Crassus666@users.noreply.github.com>
2020-11-23 23:54:11 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pozitiv_szamok_Pelsoczi_Domokos12A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sor;
+            int szam;
+            do
+            {                
+                Console.Write("Kérem írja be a számot: ");
+                sor = Console.ReadLine();
+                szam = Int32.Parse(sor);
+                if (szam <= 0)
+                {
+                    Console.WriteLine("Pozitív számot írjon be!");
+                }
+            } 
+            while (0 < szam );
+
+        }
+    }
+}
