382f11d67d21e40469c59361a6401d50e5399fe0
Crassus666 <74691874+Crassus666@users.noreply.github.com>
2020-11-23 23:49:16 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace primszame_Pelsoczi_Domokos_12A
+{
+    class Program
+    {
+        static bool prim_e(int a)
+        {
+            int i = 2;
+            double gyok = Math.Round(Math.Sqrt(a));
+            while (a % i != 0 && i <= gyok)
+            {
+                i++;
+            }
+            if (i > gyok)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main()
+        {
+            Console.WriteLine("A szám: ");
+            string line = Console.ReadLine();
+            int a = Convert.ToInt32(line);
+            if (prim_e(b))
+            {
+                Console.WriteLine(a + " prímszám.");
+            }
+            else
+            {
+                Console.WriteLine(a + " nem prímszám.");
+            }
+            Console.ReadKey();
+            
+        }
+}
