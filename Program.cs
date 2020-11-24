d8bb60960510b5543d662b42b75f1aef35bd065e
Crassus666 <74691874+Crassus666@users.noreply.github.com>
2020-11-24 02:57:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace kozos_oszto_Pelsoczi_Domokos12A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, szam1, szam2, j, eredmeny = 1;
+
+            Console.Write("Kérem adja meg az első számot: ");
+            szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kérem adja meg a második számot: ");
+            szam2 = Convert.ToInt32(Console.ReadLine());
+
+            if (szam1 < szam2)
+                j = szam1;
+            else
+                j = szam2;
+            for (i = 1; i <= j; i++)
+            {
+                if (szam1 % i == 0 && szam2 % i == 0)
+                {
+                    eredmeny = i;
+                }
+            }
+            Console.Write("\nA legnagyobb közös osztó a(z) {0}\n\n",eredmeny);
+        }
+    }
+}
