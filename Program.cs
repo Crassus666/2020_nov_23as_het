a72d07f45a58bdb9c59f8c60af65225c81b40329
Crassus666 <74691874+Crassus666@users.noreply.github.com>
2020-11-24 01:04:16 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,25 +1,23 @@
-﻿using System;
-
-namespace pozitiv_szamok_Pelsoczi_Domokos12A
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string sor;
-            int szam;
-            do
-            {                
-                Console.Write("Kérem írja be a számot: ");
-                sor = Console.ReadLine();
-                szam = Int32.Parse(sor);
-                if (szam <= 0)
-                {
-                    Console.WriteLine("Pozitív számot írjon be!");
-                }
-            } 
-            while (0 < szam );
-
-        }
-    }
-}
+﻿using System;  
+  public class primszame_Pelsoczi_Domokos_12A
+{  
+     public static void Main(string[] args)  
+      {
+          int szam, i, kettovel_osztva = 0;
+          bool prim=true;    
+          Console.Write("Adja meg a számot: ");    
+          szam = int.Parse(Console.ReadLine());  
+          kettovel_osztva=szam/2;    
+          for(i = 2; i <= kettovel_osztva; i++)    
+          {    
+           if(szam % i == 0)    
+            {        
+             prim=false;    
+            }    
+          }    
+          if (prim==true)    
+            Console.Write("A szám prím.");
+          else
+            Console.Write("A szám nem prím."); 
+     }  
+   }  
